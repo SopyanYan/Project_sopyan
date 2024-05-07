cebc964639bc7ffb1ea23a4a67d01b83226683c9
SopyanYan <161932842+SopyanYan@users.noreply.github.com>
2024-05-07 13:19:19 +0700

    Add files via upload

---
+++ database/migrations/2024_05_06_095013_create_sekolahs_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sekolahs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_sekolah');
+            $table->string('nama_kepala_sekolah');
+            $table->integer('umur');
+            $table->text('alamat_sekolah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sekolahs');
+    }
+};
---
+++ database/migrations/2024_05_06_095708_create_mapels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('mapels', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->boolean('jenis_kelamin');
+            $table->string('nama_pelajaran');
+            $table->integer('umur');
+            $table->text('alamat_rumah');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('mapels');
+    }
+};
